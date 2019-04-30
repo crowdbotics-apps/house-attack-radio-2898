df2dda2c9a4946bb5e7ac3be9f5f9620c581e5ce
Crowdbotics <team@crowdbotics.com>
2019-04-30 13:28:12 +0000

    initial commit

---
+++ android/app/src/main/java/com/house_attack_radio_2898/MainActivity.java
@@ -0,0 +1,15 @@
+package com.house_attack_radio_2898;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "house_attack_radio_2898";
+    }
+}
